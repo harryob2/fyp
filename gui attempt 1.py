b62f7eecabcaadbdf8beb955b0d8a932327ce44d
haz <obrienh9@tcd.ie>
2022-02-07 18:06:24 +0000

    added ability to export to csv. 2 hours

--- gui attempt 1.py
+++ gui attempt 1.py
@@ -1,13 +1,19 @@
 import csv
 import os
 from tkinter import *
+from tkinter import filedialog
 from tkinter import ttk
+import pandas as pd
+
 
 ##what to do next
 #need to figure out how to export data to csv, everything else is cosmetic
 
 #global variable for data to export
-user_data = []
+column_headings = ['Stage', 'Blood Lactate', 'Velocity']
+user_data = pd.DataFrame(columns = column_headings)
+#user_data = user_data.append(column_headings)
+print(user_data)
 
 root = Tk()
 root.title('FYP GUI')
@@ -32,9 +38,6 @@ user_data_tree.heading('Stage', text = 'Stage')
 user_data_tree.heading('Blood Lactate', text = 'Blood Lactate')
 user_data_tree.heading('Velocity', text = 'Velocity')
 
-
-#create csv file, to which we will add user input
-user_data_open = open('user_data.csv', 'w', newline = '')
 
 #define variables
 stage_var = StringVar()
@@ -70,26 +73,37 @@ def get_text():
     user_data_tree.insert(index='end', values = row_list, parent='')
     iid += 1
 
+    global user_data
+    #user_data = user_data.append(row_list, ignore_index = True)
+    df_length = len(user_data)
+    user_data.loc[df_length] = row_list
+    print(user_data)
     #clear list values
     row_list.clear()
-
-
-#display the user input as a label
-    stage_label.config(text=stage)
-    blood_lactate_label.config(text=blood_lactate)
-    velocity_label.config(text=velocity)
 
 #empty the text boxes
     stage_textbox.delete(0, END)
     blood_lactate_textbox.delete(0, END)
     velocity_textbox.delete(0, END)
 
+    #stage_label.config(text=stage)
+    #blood_lactate_label.config(text=blood_lactate)
+    #velocity_label.config(text=velocity)
+
+
+def export():
+    user_data.to_csv(filedialog.asksaveasfilename(title="Export to CSV", filetypes=(("CSV File", "*.csv"), ("All Files", "*.*")), defaultextension=".csv"), index=False)
+
+#display the user input as a label
+
+
 
 ##create button
 button_frame = Frame(root)
 
-get_text_button = Button(button_frame, text="Submit", command = get_text)
-get_text_button.grid(row=1, column=1, padx=5)
+get_text_button = Button(wrapper1, text="Submit", command = get_text)
+#get_text_button.grid(row=1, column=1, padx=5)
+export_button = Button(wrapper2, text="Export", command = export)
 
 
 #labels
@@ -105,38 +119,21 @@ wrapper2.pack(fill='both', expand='yes', padx=20, pady=20)
 
 stage_title_label.pack()
 stage_textbox.pack(pady=5, padx=5)
-stage_label.pack(pady=1)
 
 blood_lactate_title_label.pack(pady=5)
 blood_lactate_textbox.pack(pady=5)
-blood_lactate_label.pack(pady=1)
 
 velocity_title_label.pack(pady=5)
 velocity_textbox.pack(pady=5)
-velocity_label.pack(pady=1)
 
-button_frame.pack(pady=5)
+get_text_button.pack(pady=10)
+
+export_button.pack(pady=10)
 
 user_data_tree.pack()
 #---------------------
 
 
-
-file = open('C:\\Users\\harry\\OneDrive\\Documents\\college\\fyp\\data.csv', 'w', newline='')
-
-header = ['stage', 'power', 'blood lactate']
-
-data = ['1', '100', '1.1']
-
-data2 = ['2', '120', '1.2']
-
-writer = csv.writer(file)
-writer.writerow(header)
-writer.writerow(data)
-writer.writerow(data2)
-
-
-file.close()
 
 
 root.mainloop()
