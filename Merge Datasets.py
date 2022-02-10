81a32e84f287fa33c0ff5591d77e55b668e31ddb
haz <obrienh9@tcd.ie>
2022-02-10 16:57:12 +0000

    fixed buttons that import datasets, now I only need to merge the datasets and write the data visualization code

--- Merge Datasets.py
+++ Merge Datasets.py
@@ -9,21 +9,44 @@ root = Tk()
 root.title('FYP GUI - Merge Datasets')
 root.geometry("450x550")
 
-df = pandas.DataFrame()
+HR_df = pandas.DataFrame()
+GUI_df = pandas.DataFrame()
 
-# must fix this, wont import xlsx files due to encoding error
-def thing():
-    df = pandas.read_excel(filedialog.askopenfile(title = "Import Athlete VO2/HR Data"))
-    print(df)
+def import_HR():
+    HR_file_path = filedialog.askopenfilename(title = "Import Athlete HR Data")
+    HR_df = pandas.read_csv(HR_file_path)
+    HR_file_name = os.path.basename(HR_file_path)
+    HR_file_name_label = Label(wrapper1, text = HR_file_name)
+    HR_file_name_label.pack()
+
+def import_GUI():
+    GUI_file_path = filedialog.askopenfilename(title = "Import User Inputed Data")
+    GUI_df = pandas.read_csv(GUI_file_path)
+    GUI_file_name = os.path.basename(GUI_file_path)
+    GUI_file_name_label = Label(wrapper2, text = GUI_file_name)
+    GUI_file_name_label.pack()
+
+def run_program():
+    print("hello world")
+
+
 
 button_frame = Frame(root)
 
-wrapper1 = LabelFrame(root, text = 'Current Data')
+wrapper1 = LabelFrame(root, text = 'HR Data')
+wrapper2 = LabelFrame(root, text = 'GUI Data')
+wrapper3 = LabelFrame(root, text = 'Run Program')
 
-import_button = Button(wrapper1, text = "Import", command = thing)
+import_HR_button = Button(wrapper1, text = "Import HR Data", command = import_HR)
+import_GUI_button = Button(wrapper2, text = "Import GUI Data", command = import_GUI)
+run_program_button = Button(wrapper3, text = "Run Program", command = run_program)
 
 wrapper1.pack(fill='both', expand='yes', padx=20, pady=20)
+wrapper2.pack(fill='both', expand='yes', padx=20, pady=20)
+wrapper3.pack(fill='both', expand='yes', padx=20, pady=20)
 
-import_button.pack(pady=10)
+import_HR_button.pack(pady=10)
+import_GUI_button.pack(pady=10)
+run_program_button.pack(pady=10)
 
 root.mainloop()
